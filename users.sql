8dcaa75e2eb6530095286521411c722dcd1c6463
Vishwa Dipakbhai Parmar <parmarvishwa2015@gmail.com>
2026-04-30 16:35:59 +0530

    Create users.sql

---
+++ users.sql
@@ -0,0 +1,18 @@
+
+USE gold_db;
+ 
+CREATE TABLE customersNew (
+
+    id INT PRIMARY KEY,
+
+    name VARCHAR(100),
+
+    nickname VARCHAR(100),
+
+    logo VARCHAR(40),
+
+    email VARCHAR(100),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
